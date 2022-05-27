9fc384b160f2004b2fc8af9741fc31d6202f03ac
Nguyen Huu Huy <huytranduong78@gmail.com>
2022-05-27 17:17:11 +0700

    add lazy-loading slider

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,30 @@ var mySwiper = new Swiper(".swiper-container", {
   },
   loop: true,
 });
+
+function loadingImagesLazy(img) {
+  const url = img.getAttribute("lazy-src");
+  img.setAttribute("src", url);
+}
+
+function ready() {
+  if ("IntersectionObserver" in window) {
+    // support
+    var lazyImages = document.querySelectorAll("[lazy-src]");
+
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          loadingImagesLazy(entry.target);
+        }
+      });
+    });
+
+    lazyImages.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    //get bounding
+  }
+}
+document.addEventListener("DOMContentLoaded", ready);
